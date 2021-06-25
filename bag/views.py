243ef7829c89af0f51341bd4005f516998b42618
wyne-ybanez <wyne_01@icloud.com>
2021-06-25 00:59:01 +0000

    Added shopping bag app, URLs and template

---
+++ bag/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def view_bag(request):
+    """
+    A view to return the shopping bag
+    """
+    return render(request, 'bag/bag.html')
