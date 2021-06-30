600e0239abc54e125fcde6606b370056f7a02fc6
wyne-ybanez <wyne_01@icloud.com>
2021-06-30 16:30:29 +0000

    Finished Product size logic

--- bag/views.py
+++ bag/views.py
@@ -18,14 +18,32 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    # if item_id is one of the keys, increase quantity
-    # bag's item_id key is the quantity, if there is already product key, increase quantity
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    # If there is a size
+    # If item already in the bag, we check if item ID and same size exists
+    # We then increase this if the same item and size is added, 
+    # else, it equals quantity as same item new size
+    # Else if no recorded item ID, just arrange according to size
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    else: 
+        # If there is no size
+        # if item_id is one of the keys, increase quantity
+        # bag's item_id key is the quantity, if there is already product key, increase quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
