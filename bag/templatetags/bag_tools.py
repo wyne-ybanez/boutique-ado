34c39abd4cfee466b3312aef768dda6433d7a0bf
wyne-ybanez <wyne_01@icloud.com>
2021-07-02 14:58:39 +0000

    Restructured bag_tools coding positions and added useful comments to explain templating and filter process

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -1,8 +1,11 @@
 from django import template
 
-
 register = template.Library()
+
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
+    """
+    For the front end to calculate the price
+    """
     return price * quantity
