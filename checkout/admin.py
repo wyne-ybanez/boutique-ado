d65690fa4f23cff819070ca1bb11a64f95eb5f0a
wyne-ybanez <wyne_01@icloud.com>
2021-07-04 20:11:58 +0000

    Added forms, admin and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    This inline item is going to allow us to add and edit line items in 
+    the admin right from inside the order model.
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    # These fields are all things that will be calculated by our model methods.
+    # So we don't want anyone to have the ability to edit them.
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    # Allow us to specify the order of the fields in the admin interface.
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    # To restrict the columns that show up in the order list to only a few key items.
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
