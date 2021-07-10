f97b76cacef2e473775de90e4956db53ab306a6d
wyne-ybanez <wyne_01@icloud.com>
2021-07-10 00:26:08 +0000

    completed webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -18,14 +18,16 @@ class OrderAdmin(admin.ModelAdmin):
     # So we don't want anyone to have the ability to edit them.
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     # Allow us to specify the order of the fields in the admin interface.
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
     # To restrict the columns that show up in the order list to only a few key items.
     list_display = ('order_number', 'date', 'full_name',
